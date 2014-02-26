e903df21acee55d27d22d72aaf5556c20e0920f1
david.bashford <dbashford@bericotechnologies.com>
2014-02-26 11:11:17 -0500

    updating, adding error callbacks

--- src/index.js
+++ src/index.js
@@ -5,6 +5,27 @@ var fs = require( "fs" )
   , mkdirp = require( "mkdirp" )
   , config = require( "./config" );
 
+var _copy = function( source, target, cb ) {
+  var cbCalled = false;
+
+  function done( err ) {
+    if ( !cbCalled ) {
+      cb( err );
+      cbCalled = true;
+    }
+  }
+
+  var rd = fs.createReadStream( source );
+  rd.on( "error", done );
+
+  var wr = fs.createWriteStream( target );
+  wr.on( "error", done );
+  wr.on( "close", function( ex ) {
+    done();
+  });
+  rd.pipe( wr );
+};
+
 var _process = function ( mimosaConfig, options, next ) {
   if ( options.files && options.files.length ) {
     options.files.forEach( function( file, i ) {
@@ -13,12 +34,19 @@ var _process = function ( mimosaConfig, options, next ) {
         if ( err ) {
           mimosaConfig.log.error( "Could not create directory, " + err );
         } else {
-          fs.createReadStream( file.inputFileName ).pipe( fs.createWriteStream( out ) );
-          mimosaConfig.log.success( "Copied file [[ " + out + " ]]" );
-        }
-        if ( i + 1 === options.files.length) {
-          // work is done, call it a day
-          next( false );
+          var cb = function( err ) {
+            if ( err ) {
+              mimosaConfig.log.error( "Encountered stream error ", err, { exitIfBuild:true } );
+            } else {
+              mimosaConfig.log.success( "Copied file [[ " + out + " ]]" );
+            }
+
+            if ( i + 1 === options.files.length) {
+              // work is done, call it a day
+              next( false );
+            }
+          };
+          _copy( file.inputFileName, out, cb );
         }
       });
     });
@@ -28,7 +56,7 @@ var _process = function ( mimosaConfig, options, next ) {
 };
 
 var registration = function ( mimosaConfig, register ) {
-  register( [ "buildFile", "add", "update"], "init", _process, mimosaConfig.streamCopy.extensions );
+  register( [ "buildFile", "add", "update"], "beforeRead", _process, mimosaConfig.streamCopy.extensions );
 };
 
 module.exports = {
